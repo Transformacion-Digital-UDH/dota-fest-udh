3507f7367be0a6d317a5a5910c07996f6abc257e
Kevin Arnold Flores Pacheco <arnold_k2001@hotmail.com>
2025-05-27 12:15:25 -0500

    add: update Astro configuration, add @astrojs/node dependency, and modify Welcome component to use new video asset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+  server: {
+    host: true,
+    port: 3011
+  },
+  adapter: node({mode: 'standalone'}),
+  output: 'server'
 });
